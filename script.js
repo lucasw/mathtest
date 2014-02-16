9dca058436929ffde75db68a38d6f99331c881ea
lucasw <lucasw_backwards@gmail.com>
2014-02-16 06:19:36 -0800

    Can click on numbers and see debug output on console, but have to click precisely on the black text, not the white around them, so need to have a square button behind number.

--- script.js
+++ script.js
@@ -18,8 +18,64 @@
  
  */
 
+var stage;
+var numpad;
+var wd;
+var ht;
+var button_size;
+
+function numClick(evt, data) {
+
+  console.log("clicked on " + data.num);
+}
+
+function NumButton(x, y, num, parent_container) {
+  var msg = new createjs.Text("", "1px Courier", "#111");
+  var num = num;
+
+  msg.scaleX = button_size / 16;
+  msg.scaleY = button_size / 16;
+  msg.text = num.toString(16);
+  msg.x = x;
+  msg.y = y;
+  parent_container.addChild(msg);
+
+  var listener = msg.on("click", numClick, null, false, {num:num});
+}
+
+function NumPad(base, parent_container) {
+  var number_buttons = [];
+
+  var base = base;
+
+  container = new createjs.Container();
+  parent_container.addChild(container);
+  
+  var buttons_per_column = Math.floor(ht / button_size);
+  var x_start = wd - button_size * Math.ceil(base / buttons_per_column);
+
+  for (var i = 0; i < base; i++) {
+    var butto
+    var x = x_start + button_size * Math.floor(i / buttons_per_column);
+    var y = (i % buttons_per_column) * button_size; 
+    var num_button = NumButton(x, y, i, container);
+
+  }
+}
 
 function init() {
+  stage = new createjs.Stage("mathtest");
 
+  wd = stage.canvas.width;
+  ht = stage.canvas.height;
+  button_size = ht/10;
 
+  var context = stage.canvas.getContext("2d");
+  context.imageSmoothingEnabled = false;
+  context.mozImageSmoothingEnabled = false;
+  context.webkitImageSmoothingEnabled = false;
+
+  numpad = NumPad(10, stage);
+
+  stage.update();
 }
